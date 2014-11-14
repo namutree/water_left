6169258007bb07a9ab08be018209cba8570ba66e
John N Choi <jnc331@nyu.edu>
2014-11-14 01:38:42 -0500

    first commit

---
+++ xcode/build/water_left.build/Debug/water_left.build/Objects-normal/i386/water_leftApp.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/johnnamuchoi/Documents/2014\ Fall\ ITP/Big\ Screen/cinder/water_left/src/water_leftApp.cpp \
+  ../../../../../cinder/cinder_0.8.6_mac/include/cinder/app/AppNative.h \
+  ../../../../../cinder/cinder_0.8.6_mac/include/cinder/gl/Texture.h \
+  /Users/johnnamuchoi/Documents/2014\ Fall\ ITP/Big\ Screen/cinder/water_left/xcode/WaterdropCon.h \
+  ../../../../../cinder/cinder_0.8.6_mac/include/cinder/Rand.h \
+  /Users/johnnamuchoi/Documents/2014\ Fall\ ITP/Big\ Screen/cinder/water_left/xcode/Waterdrop.h
